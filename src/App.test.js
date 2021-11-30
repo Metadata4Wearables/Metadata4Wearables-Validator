1a95a8a0bbd25b343d1e42c84886afcff053dced
Chris Lowis <chris.lowis@gmail.com>
2021-11-30 11:24:56 +0000

    Test download link appears when submitting valid study name
    Co-authored-by: Chris Roos <chris.roos@gofreerange.com>
    Co-authored-by: James Mead <james@floehopper.org>

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,16 @@
 import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import App from "./App";
 
-test("renders form with name field", () => {
+test("user can download after submitting a study with a valid name", async () => {
+  global.URL.createObjectURL = jest.fn().mockReturnValue("https://example.com");
+
   render(<App />);
   const nameField = screen.getByLabelText(/name/);
-  expect(nameField).toBeInTheDocument();
+  userEvent.type(nameField, "Study Name");
+  const submitButton = screen.getByRole("button", { name: /submit/i });
+  userEvent.click(submitButton);
+
+  const downloadLink = await screen.findByRole("link", { name: "Download" });
+  expect(downloadLink).toBeInTheDocument();
 });

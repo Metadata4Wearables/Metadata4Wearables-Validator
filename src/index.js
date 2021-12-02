2856f2c95a3c826324e1713c6fb3b7e1755a5833
James Mead <james@floehopper.org>
2021-12-02 11:28:45 +0000

    Push logic down a level from index.js -> App.js & App.js -> Study.js
    We're hoping this will make it easier to introduce a form for the
    participant(s).
    
    Co-authored-by: Chris Roos <chris.roos@gofreerange.com>
    Co-authored-by: Chris Lowis <chris.lowis@gmail.com>

--- src/index.js
+++ src/index.js
@@ -2,14 +2,5 @@ import "bootstrap/dist/css/bootstrap.css";
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import Nav from "./Nav";
 
-ReactDOM.render(
-  <div className="container">
-    <Nav />
-    <div className="container-fluid">
-      <App />
-    </div>
-  </div>,
-  document.getElementById("root")
-);
+ReactDOM.render(<App />, document.getElementById("root"));

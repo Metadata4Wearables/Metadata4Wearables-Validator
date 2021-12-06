1f7d9fce7674bc66f0e71c6534e5c535a030f16d
Chris Roos <chris@seagul.co.uk>
2021-12-06 16:31:13 +0000

    Use ...ByRole in our tests
    The Testing Library docs explain that we should almost always use these
    `ByRole` query functions[1]
    
    [1]: https://testing-library.com/docs/queries/about#priority

--- src/App.test.js
+++ src/App.test.js
@@ -10,22 +10,26 @@ test("user can download after submitting a study with a valid name", async () =>
   const studyLink = screen.getByRole("link", { name: /study/i });
   userEvent.click(studyLink);
 
-  const nameField = screen.getByLabelText(/name/i);
+  const nameField = screen.getByRole("textbox", { name: /name/i });
   userEvent.type(nameField, "Study Name");
 
   const clinicalTrial = screen.getByRole("radio", { name: /yes/i });
   userEvent.click(clinicalTrial);
 
-  const clinicalTrialID = await screen.findByLabelText(/clinical trial id/i);
+  const clinicalTrialID = await screen.findByRole("textbox", {
+    name: /clinical trial id/i,
+  });
   userEvent.type(clinicalTrialID, "Clinical Trial ID");
 
-  const ethicsInformation = screen.getByLabelText(/ethics information/i);
+  const ethicsInformation = screen.getByRole("textbox", {
+    name: /ethics information/i,
+  });
   userEvent.type(ethicsInformation, "Ethics Information");
 
-  const sample = screen.getByLabelText(/sample/i);
+  const sample = screen.getByRole("textbox", { name: /sample/i });
   userEvent.type(sample, "Sample");
 
-  const description = screen.getByLabelText(/description/i);
+  const description = screen.getByRole("textbox", { name: /description/i });
   userEvent.type(description, "Description");
 
   const keywordsGroup = screen.getByRole("group", { name: /keywords/i });
